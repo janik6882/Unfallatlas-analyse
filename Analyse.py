79847d2e5fc0052c7716ad5fa800821d18823a49
Janik <janik@klauenberg.eu>
2020-10-11 20:15:41 +0200

    night commit

--- Analyse.py
+++ Analyse.py
@@ -1,22 +1,30 @@
 import csv
 import os
 import json
-def extract_loc(path):
+def extract_loc(path, year, type=0):
     f = open(path, mode="r")
     reader = csv.reader(f, delimiter=";")
     data = list(reader)[1:]
-    loc_data = [[float(i[-2].replace(",", ".")), float(i[-1].replace(",", "."))] for i in data]
+    if type==0:
+        loc_data = [[float(i[-2].replace(",", ".")), float(i[-1].replace(",", ".")), year] for i in data]
+    elif type==1:
+        loc_data = [[float(i[-3].replace(",", ".")), float(i[-2].replace(",", ".")), year] for i in data]
     return loc_data
-
+def out_to_csv(data, filename):
+    filename = filename + ".csv"
+    with open(filename, "wb") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 pth1 = os.path.join("Daten", "Unfallorte2016_EPSG25832_CSV", "csv", "Unfallorte_2016_LinRef.csv")
 pth2= os.path.join("Daten", "Unfallorte2017_EPSG25832_CSV", "csv", "Unfallorte2017_LinRef.csv")
 pth3 = os.path.join("Daten", "Unfallorte2018_EPSG25832_CSV", "csv", "Unfallorte2018_LinRef.csv")
 pth4 = os.path.join("Daten", "Unfallorte2019_EPSG25832_CSV", "csv", "Unfallorte2019_LinRef.csv")
-data1 =  extract_loc(pth1)
-data2 = extract_loc(pth2)
-data3 = extract_loc(pth3)
-data4 = extract_loc(pth4)
+data1 =  extract_loc(pth1, 2016)
+data2 = extract_loc(pth2, 2017)
+data3 = extract_loc(pth3, 2018)
+data4 = extract_loc(pth4, 2019, type=1)
 data = data1 + data2 + data3 + data4
-data.insert(0, ["lng", "lat"])
+data.insert(0, ["lng", "lat", "year"])
 print data[0:5]
 json.dump(data, open("out.json", "w"))
+out_to_csv(data4, "out")
--- Auswertung.py
+++ Auswertung.py
@@ -4,39 +4,47 @@ import pandas as pd
 MAPBOX_API_KEY = json.load(open("tokens.json", "r"))["pub_tok"]
 import pydeck as pdk
 data = json.load(open("out.json", "r"))
-df = pd.read_csv((os.path.join("Daten", "Unfallorte2016_EPSG25832_CSV", "csv", "Unfallorte_2016_LinRef.csv")), sep="\t", lineterminator="\n", decimal=",")
-#df = df[:-1]
-#df.columns = ["lng", "lat"]
+df = pd.read_csv("out.csv", sep="\n", lineterminator="\n", delimiter=",")
+df = df[:-1]
 data = data[1:]
-print data[0:5]
+df.columns = ["lng", "lat", "val"]
+print df
 #Daten\Unfallorte2016_EPSG25832_CSV\csv\Unfallorte_2016_LinRef.csv
-"""layer = pdk.Layer(
+layer = pdk.Layer(
     'ScatterplotLayer',
-    data,
-    get_position="-",
+    df,
+    get_position=["lng", "lat", "val"],
     auto_highlight=True,
     pickable=True,
-    get_radius=1000,
+    get_radius=500,
     get_fill_color=[180, 0, 200, 140],
     #elevation_scale=50,
     #elevation_range=[0, 3000],
     #extruded=True,
     #coverage=1
-    )"""
+    )
+
 layer2 = pdk.Layer(
     'HexagonLayer',
-    data,
-    get_position="-",
+    df,
+    get_position=["lng","lat", "val"],
     auto_highlight=True,
-    elevation_scale=250,
+    elevation_scale=50,
     pickable=True,
     elevation_range=[0, 3000],
     extruded=True,
-    coverage=0.5,
+    coverage=1,
     )
 view_state = pdk.ViewState(
     longitude=13.30511, latitude=52.49548, zoom=6, min_zoom=5, max_zoom=15, pitch=40.5, bearing=-27.36,
 )
 
-r = pdk.Deck(layers=[layer2], initial_view_state=view_state, mapbox_key=MAPBOX_API_KEY)
+r = pdk.Deck(layers=[layer], initial_view_state=view_state,
+            mapbox_key=MAPBOX_API_KEY,
+            tooltip={'html': '<b>Elevation Value:</b> {elevationValue}',
+                     'style': {
+                     'color': 'white'
+                               }
+                    }
+            )
 r.to_html("hexagon_layer.html")
