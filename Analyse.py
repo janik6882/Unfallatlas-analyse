51b4891dcaa17c8087ea1b2d3a342b65e6b5e3b7
Janik <janik@klauenberg.eu>
2020-10-11 14:33:17 +0200

    minor changes

--- Analyse.py
+++ Analyse.py
@@ -8,8 +8,15 @@ def extract_loc(path):
     loc_data = [[float(i[-2].replace(",", ".")), float(i[-1].replace(",", "."))] for i in data]
     return loc_data
 
-pth = os.path.join("Daten", "Unfallorte2017_EPSG25832_CSV", "csv", "Unfallorte2017_LinRef.csv")
-data =  extract_loc(pth)
+pth1 = os.path.join("Daten", "Unfallorte2016_EPSG25832_CSV", "csv", "Unfallorte_2016_LinRef.csv")
+pth2= os.path.join("Daten", "Unfallorte2017_EPSG25832_CSV", "csv", "Unfallorte2017_LinRef.csv")
+pth3 = os.path.join("Daten", "Unfallorte2018_EPSG25832_CSV", "csv", "Unfallorte2018_LinRef.csv")
+pth4 = os.path.join("Daten", "Unfallorte2019_EPSG25832_CSV", "csv", "Unfallorte2019_LinRef.csv")
+data1 =  extract_loc(pth1)
+data2 = extract_loc(pth2)
+data3 = extract_loc(pth3)
+data4 = extract_loc(pth4)
+data = data1 + data2 + data3 + data4
 data.insert(0, ["lng", "lat"])
 print data[0:5]
 json.dump(data, open("out.json", "w"))
--- Auswertung.py
+++ Auswertung.py
@@ -5,12 +5,12 @@ MAPBOX_API_KEY = json.load(open("tokens.json", "r"))["pub_tok"]
 import pydeck as pdk
 data = json.load(open("out.json", "r"))
 df = pd.read_csv((os.path.join("Daten", "Unfallorte2016_EPSG25832_CSV", "csv", "Unfallorte_2016_LinRef.csv")), sep="\t", lineterminator="\n", decimal=",")
-df = df[:-1]
-df.columns = ["lng", "lat"]
+#df = df[:-1]
+#df.columns = ["lng", "lat"]
 data = data[1:]
 print data[0:5]
 #Daten\Unfallorte2016_EPSG25832_CSV\csv\Unfallorte_2016_LinRef.csv
-layer = pdk.Layer(
+"""layer = pdk.Layer(
     'ScatterplotLayer',
     data,
     get_position="-",
@@ -22,21 +22,21 @@ layer = pdk.Layer(
     #elevation_range=[0, 3000],
     #extruded=True,
     #coverage=1
-    )
+    )"""
 layer2 = pdk.Layer(
     'HexagonLayer',
     data,
     get_position="-",
     auto_highlight=True,
-    elevation_scale=50,
+    elevation_scale=250,
     pickable=True,
     elevation_range=[0, 3000],
     extruded=True,
-    coverage=1,
+    coverage=0.5,
     )
 view_state = pdk.ViewState(
     longitude=13.30511, latitude=52.49548, zoom=6, min_zoom=5, max_zoom=15, pitch=40.5, bearing=-27.36,
 )
 
-r = pdk.Deck(layers=[layer, layer2], initial_view_state=view_state, mapbox_key=MAPBOX_API_KEY)
+r = pdk.Deck(layers=[layer2], initial_view_state=view_state, mapbox_key=MAPBOX_API_KEY)
 r.to_html("hexagon_layer.html")
