76c834f9ddf0b32c1e4786969881778297bbf085
Janik <janik@klauenberg.eu>
2020-10-11 11:31:37 +0200

    finished Debugging

--- Analyse.py
+++ Analyse.py
@@ -5,10 +5,10 @@ def extract_loc(path):
     f = open(path, mode="r")
     reader = csv.reader(f, delimiter=";")
     data = list(reader)[1:]
-    loc_data = [[i[-2], i[-1]] for i in data]
+    loc_data = [[float(i[-2].replace(",", ".")), float(i[-1].replace(",", "."))] for i in data]
     return loc_data
 
-pth = os.path.join("Daten", "Unfallorte2016_EPSG25832_CSV", "csv", "Unfallorte_2016_LinRef.csv")
+pth = os.path.join("Daten", "Unfallorte2017_EPSG25832_CSV", "csv", "Unfallorte2017_LinRef.csv")
 data =  extract_loc(pth)
 data.insert(0, ["lng", "lat"])
 print data[0:5]
--- Auswertung.py
+++ Auswertung.py
@@ -7,21 +7,36 @@ data = json.load(open("out.json", "r"))
 df = pd.read_csv((os.path.join("Daten", "Unfallorte2016_EPSG25832_CSV", "csv", "Unfallorte_2016_LinRef.csv")), sep="\t", lineterminator="\n", decimal=",")
 df = df[:-1]
 df.columns = ["lng", "lat"]
-print df
+data = data[1:]
+print data[0:5]
 #Daten\Unfallorte2016_EPSG25832_CSV\csv\Unfallorte_2016_LinRef.csv
 layer = pdk.Layer(
+    'ScatterplotLayer',
+    data,
+    get_position="-",
+    auto_highlight=True,
+    pickable=True,
+    get_radius=1000,
+    get_fill_color=[180, 0, 200, 140],
+    #elevation_scale=50,
+    #elevation_range=[0, 3000],
+    #extruded=True,
+    #coverage=1
+    )
+layer2 = pdk.Layer(
     'HexagonLayer',
-    [[[13.30511, 52.46556]]],
-    get_position=['lng', 'lat'],
+    data,
+    get_position="-",
     auto_highlight=True,
     elevation_scale=50,
     pickable=True,
     elevation_range=[0, 3000],
     extruded=True,
-    coverage=1)
+    coverage=1,
+    )
 view_state = pdk.ViewState(
     longitude=13.30511, latitude=52.49548, zoom=6, min_zoom=5, max_zoom=15, pitch=40.5, bearing=-27.36,
 )
 
-r = pdk.Deck(layers=[layer], initial_view_state=view_state, mapbox_key=MAPBOX_API_KEY)
+r = pdk.Deck(layers=[layer, layer2], initial_view_state=view_state, mapbox_key=MAPBOX_API_KEY)
 r.to_html("hexagon_layer.html")
